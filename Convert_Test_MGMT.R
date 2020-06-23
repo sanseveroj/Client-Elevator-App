3f041288d6929238a083068e79f47c29e1ab1730
sanseveroj <sanseveroj@gmail.com>
2020-06-23 15:45:42 -0400

    New clients/buildings tables - avalon bay

--- Convert_Test_MGMT.R
+++ Convert_Test_MGMT.R
@@ -17,22 +17,24 @@ generate_id <- function() {
 
 #Get new client ####
  my_clients <- TestMgmt %>%
- filter(str_detect(`Client Name`,"AVALON")) %>% #select(`Client Name`) %>% unique()
+ filter(str_detect(`Client Name`,"AVALON")) %>%
  select(`Client Name`, `Property Manager Phone`,`Property Manager Name`) %>%
- group_by(`Client Name`) %>%
- mutate(ID_Client = generate_id()) %>%
- unique() %>%
+ separate(`Client Name`, into = c('Client Name', 'Region'), sep = "-") %>%
+ mutate(ID_Client = generate_id()) %>% 
+ group_by(`Client Name`) %>% 
+  unique() %>%
   slice(1) %>%
-  ungroup()
+ select(-Region) %>%
+  ungroup() 
  
- my_clients <- rbind.data.frame(my_clients, 
-                                cbind.data.frame(
-                                  `Client Name` = 'AVALON BAY',
-                                  `Property Manager Phone` = 860999333,
-                                  `Property Manager Name` = 'Avalon Master',
-                                  ID_Client = generate_id()
-                                   )
-                                )
+ # my_clients <- rbind.data.frame(my_clients, 
+ #                                cbind.data.frame(
+ #                                  `Client Name` = 'AVALON BAY',
+ #                                  `Property Manager Phone` = 860999333,
+ #                                  `Property Manager Name` = 'Avalon Master',
+ #                                  ID_Client = generate_id()
+ #                                   )
+ #                                )
 names(my_clients) <- c('Client', 'Phone', 'Contact','ID_Client')
 my_clients <- my_clients %>% select(ID_Client, Client, Phone, Contact)
 
@@ -42,25 +44,21 @@ client <- rbind.data.frame(client, my_clients)
 my_buildings <- TestMgmt %>%
 filter(str_detect(`Client Name`,"AVALON")) %>%
 group_by(`Street Address`) %>%
-select(`Street Address`, `DeviceCount`) %>%
+select(`Street Address`, `DeviceCount`, `Client Name`) %>%
+separate(`Client Name`, into = c('Client Name', 'Region'), sep = "-") %>% 
+select(-`Client Name`) %>% 
 mutate(ID_Building = generate_id()) %>%
 mutate(ID_Client = generate_id()) %>%
-mutate(PM.ReqHrs = sample(10:40, size = 1, replace = TRUE))
+mutate(PM.ReqHrs = sample(10:40, size = 1, replace = TRUE))%>%
 unique() %>%
   slice(1) %>%
   ungroup()
 
-my_buildings <- rbind.data.frame(my_buildings, cbind.data.frame(
-                                              `Street Address` = 'Master Address',
-                                              `DeviceCount` = 'Master',
-                                              `PM.ReqHrs` = 0,
-                                              ID_Building = generate_id(),
-                                              ID_Client = generate_id()
-))
+buildings$Region <- NA
 
-
-names(my_buildings) <- c('Address', 'Elevators', 'ID_Building', 'ID_Client', 'PM.ReqHrs')
-my_buildings <- my_buildings %>% select(ID_Building, ID_Client, Address, Elevators, PM.ReqHrs)
+names(buildings)
+names(my_buildings) <- c('Address', 'Elevators','Region', 'ID_Building', 'ID_Client', 'PM.ReqHrs')
+my_buildings <- my_buildings %>% select(ID_Building, ID_Client, Address, Elevators, PM.ReqHrs, Region)
 
 buildings <- rbind.data.frame(buildings, my_buildings)
 
